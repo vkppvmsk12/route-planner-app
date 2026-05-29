d3f57d5a601b96bf7b7ab4431db0a47ceb4bc8c3
Vihaan Kembhavi <vihaankembhavi12@gmail.com>
2026-05-29 22:24:29 +0200

    Cleaned up duration and distance output

--- src/routing_test.py
+++ src/routing_test.py
@@ -28,7 +28,15 @@ route = data["routes"][0]
 duration_seconds = route["summary"]["duration"]
 distance_meters = route["summary"]["distance"]
 
-print(f"Duration: {round(duration_seconds / 60)} min")
+
+
+match duration_seconds:
+    case d if d >= 86400:
+        print(f"Duration: {int(duration_seconds/86400)} d {round((duration_seconds/86400-int(duration_seconds/86400))*24)} h")
+    case d if 3600 <= d < 86400:
+        print(f"Duration: {int(duration_seconds/3600)} h {round((duration_seconds/3600-int(duration_seconds/3600))*60)} min")
+    case d if d < 3600:
+        print(f"Duration: {round(duration_seconds / 60)} min")
 
 match distance_meters:
     case d if d >= 10000:
@@ -36,4 +44,4 @@ match distance_meters:
     case d if 1000 <= d < 10000:
         print(f"Distance: {round(distance_meters / 1000, 1)} km")
     case d if d < 1000:
-        print(f"Distance: {round(distance_meters / 1000, 2)*1000} m")
+        print(f"Distance: {round(int(distance_meters/10)*10)} m")
